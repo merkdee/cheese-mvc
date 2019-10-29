6d9ec8d951bef34c31506e73e8f93d91ed1ca12c
Mertyce <mdoleman15@gmail.com>
2019-10-29 10:33:25 -0400

    initial commit

---
+++ src/test/java/com/example/cheesemvc/CheeseMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cheesemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeseMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
